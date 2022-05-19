6b1be9fb42ef1607ed07b13f97df0602c04429d5
Darya Kirichenko <dak3103@gmail.com>
2022-05-19 22:27:08 +0300

    task 04 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+fs.promises.readdir(path.join(__dirname, 'files')).then(data => {
+  data.forEach(file => {
+    fs.promises.stat(path.join(__dirname, 'files', file)).then(stats => {
+      if (stats.isFile()) {
+        fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+      }
+    });    
+  });
+});
